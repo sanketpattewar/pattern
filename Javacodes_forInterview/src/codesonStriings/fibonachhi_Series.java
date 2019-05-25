bccd2c2b273dfca4aa579a0b289cca0f8e3ad713
Rohan <Rohan@LAPTOP-IHOSLHHL>
2019-05-25 16:46:23 +0530

    add

--- Javacodes_forInterview/src/codesonStriings/fibonachhi_Series.java
+++ Javacodes_forInterview/src/codesonStriings/fibonachhi_Series.java
@@ -4,13 +4,32 @@ public class fibonachhi_Series {
 
 	public static void main(String[] args) {
 		
-		public static void Fibonachhinumber(int num)
-			int c ;
-			int  a=0 ;
-		        int b =1 ;
-		         int c = a+b ;
+		int a=0 ;
+	    int b=1 ;
+	    int c ;
+	      
+	    int num=10 ;
+	     
+	   
+	    System.out.print(a+" "+b);
+	    for (int i=0; i<num ;i++) {	 
+	    	
+	    	 c=a+b ;
+	    	 System.out.print(" "+c);
+	    	 a=b ;
+	    	 b=c ;
+	    	 
+	    }
+	             
+	      
+	
+}
+	
 
 
-	}
+	
+		
+
+	
 
 }
