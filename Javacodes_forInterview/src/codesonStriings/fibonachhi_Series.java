e65f9c3f5fd111199a610b9842d9a8ddb6f76f9a
rohankotalwar <42605745+rohankotalwar@users.noreply.github.com>
2019-05-25 16:27:30 +0530

    Update fibonachhi_Series.java

--- Javacodes_forInterview/src/codesonStriings/fibonachhi_Series.java
+++ Javacodes_forInterview/src/codesonStriings/fibonachhi_Series.java
@@ -3,7 +3,12 @@ package codesonStriings;
 public class fibonachhi_Series {
 
 	public static void main(String[] args) {
-
+		
+		public static void Fibonachhinumber(int num)
+			int c ;
+			int  a=0 ;
+		        int b =1 ;
+		         int c = a+b ;
 
 
 	}
